cc78f945a1aa74e56e10cda8460c47b3faff3a7b
gruenkeks <simontowskyd@gmail.com>
2025-09-07 17:48:38 +0800

    Relax ESLint rules (no-explicit-any off, unused-vars warn)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "args": "none", "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }],
+    },
+  },
 ];
 
 export default eslintConfig;
